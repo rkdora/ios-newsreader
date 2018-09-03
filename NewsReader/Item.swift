50491709ade5c801000321b19802b3d80c72b15a
Ryuto Kawano <ryuto-kawano@ie1603-237.local>
2018-09-03 16:28:14 +0900

    Complete section 2

---
+++ NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 河野 瑠斗 on 2018/09/03.
+//  Copyright © 2018年 河野 瑠斗. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
