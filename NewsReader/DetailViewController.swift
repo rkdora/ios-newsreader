b2203b553affa97fa86743d581dae5dea649a886
Ryuto Kawano <ryuto-kawano@ie1603-237.local>
2018-09-03 15:54:08 +0900

    First commit

---
+++ NewsReader/DetailViewController.swift
@@ -0,0 +1,10 @@
+//
+//  DetailViewController.swift
+//  NewsReader
+//
+//  Created by 河野 瑠斗 on 2018/09/03.
+//  Copyright © 2018年 河野 瑠斗. All rights reserved.
+//
+
+import UIKit
+
---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by 河野 瑠斗 on 2018/09/03.
+//  Copyright © 2018年 河野 瑠斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
